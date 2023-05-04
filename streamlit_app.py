aef7987ff2db39f3719b2888df79b566f7aabac8
Lingget <79074428+Lingget@users.noreply.github.com>
2023-05-04 10:32:33 +0800

    Update streamlit_app.py
    Added Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('My Parents Menu')
+streamlit.text('Blue Berry Oatmeal')
+streamlit.text('Spinach Rocky Smoothie')
+streamlit.text('Hard Boiled-egg')
