3873c909e337b0251b13117bd837e20c43b30f1e
Lingget <79074428+Lingget@users.noreply.github.com>
2023-05-04 10:35:11 +0800

    Update streamlit_app.py
    Added Emojis to the Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('My Parents Menu')
-streamlit.text('Blue Berry Oatmeal')
-streamlit.text('Spinach Rocky Smoothie')
-streamlit.text('Hard Boiled-egg')
+streamlit.text('🥣 Blue Berry Oatmeal')
+streamlit.text('🥗 Spinach Rocky Smoothie')
+streamlit.text('🐔 Hard Boiled-egg')
+streamlit.text('🥑 Avocado toast')
